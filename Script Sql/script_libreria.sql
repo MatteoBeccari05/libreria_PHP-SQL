49026a22a32e89c43bd2f5ab886f2e647604e5fd
Matteo Beccari <98811589+MatteoBeccari05@users.noreply.github.com>
2025-01-23 12:25:42 +0100

    script sql con tabella

---
+++ Script Sql/script_libreria.sql
@@ -0,0 +1,11 @@
+create database libreria;
+
+
+create table libreria.libri(
+id int primary key auto_increment,
+titolo varchar(50) not null,
+autore varchar(50) not null,
+genere varchar(50) not null,
+data_pubblicazione date not null,
+prezzo decimal
+);
